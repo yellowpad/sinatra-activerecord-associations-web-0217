68bd8ec031f4f2eb371fcb83adea5cac21ae83d9
yellowpad <zayneabraham1@gmail.com>
2017-02-22 13:07:03 -0500

    associations in active record

--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,4 +1,5 @@
 class AddColumnToCats < ActiveRecord::Migration
+ 
   def up
     add_column :cats, :owner_id, :integer
   end
@@ -6,4 +7,5 @@ class AddColumnToCats < ActiveRecord::Migration
   def down
     remove_column :cats, :owner_id
   end
+
 end
